381624d6a4723586e3ee9d4f7ecf39bcba56989f
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-04-05 15:10:08 +0200

    Fix streamability in wikipedia dataset

--- language_modelling/run_mlm.py
+++ language_modelling/run_mlm.py
@@ -453,9 +453,9 @@ def main():
             tokenized_datasets = raw_datasets.map(
                 tokenize_function,
                 batched=True,
-                num_proc=data_args.preprocessing_num_workers,
-                remove_columns=[text_column_name],
-                load_from_cache_file=not data_args.overwrite_cache,
+                # num_proc=data_args.preprocessing_num_workers,
+                # remove_columns=[text_column_name],
+                # load_from_cache_file=not data_args.overwrite_cache,
                 desc="Running tokenizer on dataset line_by_line",
             )
     else:
