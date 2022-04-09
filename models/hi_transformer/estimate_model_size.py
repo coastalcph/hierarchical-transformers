bd949e2056668a5805a14237b43c96cc10b11ee1
Ilias Chalkidis <ihalk@di.uoa.gr>
2022-04-09 12:25:24 +0200

    Fix estimate model size script

--- models/hi_transformer/estimate_model_size.py
+++ models/hi_transformer/estimate_model_size.py
@@ -1,8 +1,11 @@
 import warnings
+
 import torch
-from data import DATA_DIR
 from transformers import AutoModelForMaskedLM, AutoConfig
+
+from data import DATA_DIR
 from models.hi_transformer import HiTransformerForMaskedLM, HiTransformerConfig
+
 warnings.filterwarnings("ignore")
 
 LAYOUTS = {
@@ -10,18 +13,26 @@ LAYOUTS = {
     's2': 'S|SD|D|S|SD|D|S|SD|D',
     'p1': 'S|SD|S|SD|S|SD|S|SD',
     'p2': 'S|S|SD|S|S|SD|S|S|SD',
+    'e1': 'SD|SD|SD|S|S|S|S|S|S',
+    'e2': 'S|SD|D|S|SD|D|S|S|S|S',
+    'l1': 'S|S|S|S|S|S|SD|SD|SD',
+    'l2': 'S|S|S|S|S|SD|D|S|SD|D',
+    'b1': 'S|S|SD|D|S|SD|D|S|S|S',
+    'b2': 'S|S|SD|SD|SD|S|S|S|S',
+    'f': 'S|S|S|S|S|S|S|S|S|S|S|S',
 }
 
 
-def test_memory_usage(model):
+def test_memory_usage(model, seq_length=1024):
     torch.cuda.reset_peak_memory_stats()
     model.to('cuda')
-    input_ids = torch.zeros((1, 1024), dtype=torch.int).to('cuda')
-    attention_mask = torch.ones((1, 1024), dtype=torch.int).to('cuda')
-    optimizer = torch.optim.AdamW(lr=1e-5)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 1e-4)
-    for step, batch in range(10):
-        outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+    input_ids = torch.ones((2, seq_length), dtype=torch.long).to('cuda')
+    labels = torch.ones((2, seq_length), dtype=torch.long).to('cuda')
+    attention_mask = torch.ones((2, seq_length), dtype=torch.int).to('cuda')
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 20)
+    for _ in range(20):
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
         loss = outputs.loss
         loss.backward(loss)
         optimizer.step()
@@ -31,64 +42,79 @@ def test_memory_usage(model):
 
 
 def estimate_model_size():
-
     MAX_SENTENCE_LENGTH = 128
-    MAX_SENTENCES = 8
-    BERT_CHECKPOINT = f'google/bert_uncased_L-6_H-256_A-4'
+    CONFIGS = [{'num_hidden_layers': 6,
+                'hidden_size': 256,
+                'intermediate_size': 1024,
+                'num_attention_heads': 4},
+               {'num_hidden_layers': 6,
+                'hidden_size': 768,
+                'intermediate_size': 3072,
+                'num_attention_heads': 12}]
 
-    # load pre-trained bert model and tokenizer
-    bert_model = AutoModelForMaskedLM.from_pretrained(BERT_CHECKPOINT)
-    for layout in ['s1', 's2', 'p1', 'p2']:
-        ENCODER_LAYOUT = {}
-        for idx, block_pattern in enumerate(LAYOUTS[layout].split('|')):
-            ENCODER_LAYOUT[str(idx)] = {"sentence_encoder": True if 'S' in block_pattern else False,
-                                        "document_encoder": True if 'D' in block_pattern else False}
+    for CONFIG in CONFIGS:
+        print('-' * 150)
+        print(F'NUM LAYERS: {CONFIG["num_hidden_layers"]}\t'
+              F'NUM HIDDEN: {CONFIG["hidden_size"]}\t'
+              F'ATTENTION HEADS: {CONFIG["num_attention_heads"]}')
+        print('-' * 150)
 
-        NUM_HIDDEN_LAYERS = len(ENCODER_LAYOUT.keys())
-        # load dummy config and change specifications
-        bert_config = bert_model.config
-        htf_config = HiTransformerConfig.from_pretrained(f'{DATA_DIR}/hi-transformer')
-        # Text length parameters
-        htf_config.max_sentence_length = MAX_SENTENCE_LENGTH
-        htf_config.max_sentences = MAX_SENTENCES
-        htf_config.max_position_embeddings = MAX_SENTENCE_LENGTH
-        htf_config.model_max_length = int(MAX_SENTENCE_LENGTH * MAX_SENTENCES)
-        htf_config.num_hidden_layers = NUM_HIDDEN_LAYERS
-        # Transformer parameters
-        htf_config.hidden_size = bert_config.hidden_size
-        htf_config.intermediate_size = bert_config.intermediate_size
-        htf_config.num_attention_heads = bert_config.num_attention_heads
-        htf_config.encoder_layout = ENCODER_LAYOUT
-        # Vocabulary parameters
-        htf_config.vocab_size = bert_config.vocab_size
-        htf_config.type_vocab_size = bert_config.type_vocab_size
+        for max_sentences in [8, 16, 32]:
+            print('-' * 150)
+            print(F'MAX SEQ LENGTH: {int(max_sentences * MAX_SENTENCE_LENGTH)}')
+            print('-' * 150)
 
-        # load dummy hi-transformer model
-        htf_model = HiTransformerForMaskedLM.from_config(htf_config)
-        model_total_params = sum(p.numel() for p in htf_model.hi_transformer.parameters() if p.requires_grad)
-        model_total_params = model_total_params / 1e6
-        memory_use = test_memory_usage(htf_model)
-        print(f'Hi-transformer model with layout {layout} has {model_total_params:.1f}M number of parameters '
-              f'and {memory_use:.1f}GB peak memory use!')
+            lf_config = AutoConfig.from_pretrained('allenai/longformer-base-4096')
+            lf_config.num_hidden_layers = CONFIG['num_hidden_layers']
+            # Transformer parameters
+            lf_config.hidden_size = CONFIG['hidden_size']
+            lf_config.intermediate_size = CONFIG['intermediate_size']
+            lf_config.num_attention_heads = CONFIG['num_attention_heads']
+            # Vocabulary parameters
+            lf_config.vocab_size = 50000
+            lf_config.type_vocab_size = 2
+            lf_config.max_position_embeddings = int(MAX_SENTENCE_LENGTH * max_sentences)
+            lf_config.attention_window = [128] * CONFIG['num_hidden_layers']
+            # load dummy longformer model
+            htf_model = AutoModelForMaskedLM.from_config(lf_config)
+            model_total_params = sum(p.numel() for p in htf_model.longformer.parameters() if p.requires_grad)
+            model_total_params = model_total_params / 1e6
+            memory_use = test_memory_usage(htf_model, seq_length=lf_config.max_position_embeddings)
+            print(f'Longformer model has {model_total_params:.1f}M number of parameters '
+                   f'and {memory_use:.2f}GB peak memory use!')
+            print('-' * 150)
 
-    lf_config = AutoConfig.from_pretrained('allenai/longformer-base-4096')
-    lf_config.num_hidden_layers = NUM_HIDDEN_LAYERS
-    # Transformer parameters
-    lf_config.hidden_size = bert_config.hidden_size
-    lf_config.intermediate_size = bert_config.intermediate_size
-    lf_config.num_attention_heads = bert_config.num_attention_heads
-    # Vocabulary parameters
-    lf_config.vocab_size = bert_config.vocab_size
-    lf_config.type_vocab_size = bert_config.type_vocab_size
-    lf_config.max_position_embeddings = int(MAX_SENTENCE_LENGTH * 8)
-    lf_config.attention_window = [128] * NUM_HIDDEN_LAYERS
-    # load dummy longformer model
-    htf_model = AutoModelForMaskedLM.from_config(lf_config)
-    model_total_params = sum(p.numel() for p in htf_model.longformer.parameters() if p.requires_grad)
-    model_total_params = model_total_params / 1e6
-    memory_use = test_memory_usage(htf_model)
-    print(f'Longformer model has {model_total_params:.1f}M number of parameters '
-          f'and {memory_use:.1f}GB peak memory use!')
+            for layout in LAYOUTS:
+                ENCODER_LAYOUT = {}
+                for idx, block_pattern in enumerate(LAYOUTS[layout].split('|')):
+                    ENCODER_LAYOUT[str(idx)] = {"sentence_encoder": True if 'S' in block_pattern else False,
+                                                "document_encoder": True if 'D' in block_pattern else False}
+
+                NUM_HIDDEN_LAYERS = CONFIG['num_hidden_layers'] * 2 if layout == 'f' else CONFIG['num_hidden_layers']
+                # load dummy config and change specifications
+                htf_config = HiTransformerConfig.from_pretrained(f'{DATA_DIR}/hi-transformer')
+                # Text length parameters
+                htf_config.max_sentence_length = MAX_SENTENCE_LENGTH
+                htf_config.max_sentences = max_sentences
+                htf_config.max_position_embeddings = MAX_SENTENCE_LENGTH
+                htf_config.model_max_length = int(MAX_SENTENCE_LENGTH * max_sentences)
+                htf_config.num_hidden_layers = NUM_HIDDEN_LAYERS
+                # Transformer parameters
+                htf_config.hidden_size = CONFIG['hidden_size']
+                htf_config.intermediate_size = CONFIG['intermediate_size']
+                htf_config.num_attention_heads = CONFIG['num_attention_heads']
+                htf_config.encoder_layout = ENCODER_LAYOUT
+                # Vocabulary parameters
+                htf_config.vocab_size = 50000
+                htf_config.type_vocab_size = 2
+
+                # load dummy hi-transformer model
+                htf_model = HiTransformerForMaskedLM.from_config(htf_config)
+                model_total_params = sum(p.numel() for p in htf_model.hi_transformer.parameters() if p.requires_grad)
+                model_total_params = model_total_params / 1e6
+                memory_use = test_memory_usage(htf_model, seq_length=htf_config.model_max_length)
+                print(f'Hi-transformer model with layout {layout} has {model_total_params:.1f}M number of parameters '
+                      f'and {memory_use:.2f}GB peak memory use!')
 
 
 if __name__ == '__main__':
