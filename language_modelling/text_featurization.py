bc51432cee7290ad4a64be79911b7591683ce512
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-04-15 17:28:58 +0200

    Fix TFIDF code

--- language_modelling/text_featurization.py
+++ language_modelling/text_featurization.py
@@ -1,3 +1,7 @@
+import pickle
+import random
+
+import tqdm
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
 import warnings
@@ -10,14 +14,49 @@ def train_text_featurizer(documents, tokenizer, hidden_units=768):
         return tokenizer.tokenize(document)
 
     # init tfidf vectorizer
+    vocab = [(key, value) for (key, value) in tokenizer.vocab.items()]
+    vocab = sorted(vocab, key=lambda tup: tup[1])
+    vocab = [key for (key, value) in vocab]
     tfidf_vectorizer = TfidfVectorizer(lowercase=False, preprocessor=None, tokenizer=tokenize,
-                                       vocabulary=list(tokenizer.vocab.keys()))
+                                       vocabulary=vocab)
     pca_solver = PCA(n_components=hidden_units)
 
     tfidf_scores = tfidf_vectorizer.fit_transform(documents)
-    pca_solver.fit(tfidf_scores.toarray())
+    print('TFIDF-VECTORIZER DONE!')
 
-    return tfidf_vectorizer, pca_solver
+    pca_solver.fit(tfidf_scores.toarray())
+    print('PCA SOLVER DONE!')
+
+    with open('./data/wikipedia-dataset/tifidf_vectorizer.pkl', 'wb') as fin:
+        pickle.dump(tfidf_vectorizer, fin)
+    print('TFIDF-VECTORIZER SAVED!')
+
+    with open('./data/wikipedia-dataset/pca_solver.pkl', 'wb') as fin:
+        pickle.dump(pca_solver, fin)
+    print('PCA SOLVER SAVED!')
+
+
+def learn_idfs(documents, tokenizer):
+
+    def tokenize(document: str):
+        return tokenizer.tokenize(document)
+
+    # init tfidf vectorizer
+    vocab = [(key, value) for (key, value) in tokenizer.vocab.items()]
+    vocab = sorted(vocab, key=lambda tup: tup[1])
+    vocab = [key for (key, value) in vocab]
+    tfidf_vectorizer = TfidfVectorizer(lowercase=False, preprocessor=None, tokenizer=tokenize,
+                                       vocabulary=vocab)
+
+    tfidf_vectorizer.fit(documents)
+
+    with open('./data/wikipedia-dataset/idf_scores.pkl', 'wb') as file:
+        pickle.dump(tfidf_vectorizer.idf_, file)
+    print('IDFs SAVED!')
+
+    # with open('./data/wikipedia-dataset/idf_terms.pkl', 'wb') as file:
+    #     idf_terms = {term: idx for term, idx in enumerate(tfidf_vectorizer.get_feature_names_out())}
+    #     pickle.dump(idf_terms, file)
 
 
 if __name__ == '__main__':
@@ -25,10 +64,15 @@ if __name__ == '__main__':
     from datasets import load_dataset
 
     # load dataset
-    dataset = load_dataset("../data/wikipedia-dataset", split='train', streaming=True)
-
-    CUSTOM_TOK_FOLDER = '../data/custom-tokenizer'
+    dataset = load_dataset("./data/wikipedia-dataset", split='train')
+    dataset = dataset['text']
+    subset = random.sample(range(len(dataset)), k=500000)
+    CUSTOM_TOK_FOLDER = 'google/bert_uncased_L-6_H-256_A-4'
+    dataset_small = []
+    for i in tqdm.tqdm(subset):
+        dataset_small.append(dataset[i])
 
     # re-load tokenizer and test
-    tokenizer = AutoTokenizer.from_pretrained(CUSTOM_TOK_FOLDER)
-    tfidf_vect, pca_solver = train_text_featurizer(documents=documents, tokenizer=tokenizer)
+    tokenizer = AutoTokenizer.from_pretrained(CUSTOM_TOK_FOLDER,  model_max_length=1024)
+    train_text_featurizer(documents=dataset_small, tokenizer=tokenizer, hidden_units=256)
+
