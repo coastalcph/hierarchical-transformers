1b4c94cf949ddb4d31c9d6b06a6910c74db650d2
kiddothe2b <ilias.chalkidis@di.ku.dk>
2022-08-24 14:57:50 +0300

    Support Longformer and BigBird pre-trained models

--- evaluation/run_document_classification.py
+++ evaluation/run_document_classification.py
@@ -310,7 +310,6 @@ def main():
         )
         model = AutoModelForSequenceClassification.from_pretrained(
             model_args.model_name_or_path,
-            pooling=model_args.pooling,
             from_tf=bool(".ckpt" in model_args.model_name_or_path),
             config=config,
             cache_dir=model_args.cache_dir,
